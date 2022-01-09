3a9506cad3fa584a67df2315d64cf0dc8d5fae53
awenzelhuemer <43669899+Superandi1234@users.noreply.github.com>
2022-01-09 12:08:21 +0100

    Add restaurant title fragment

---
+++ fragments/restaurant-title.js
@@ -0,0 +1,21 @@
+class RestaurantTitle extends HTMLElement {
+
+    static get observedAttributes() {
+        return ["restaurant-id"];
+    }
+
+    connectedCallback() {
+        this.attachShadow({ mode: "open" });
+    }
+
+    attributeChangedCallback() {
+        const restaurantId = +this.getAttribute("restaurant-id");
+        fetch(`//localhost:5000/restaurant/${restaurantId}`)
+        .then(response => response.json())
+        .then(data => {
+            this.shadowRoot.innerHTML =  `<div class="text-center">${data.name}</div>`;
+        });
+    }
+}
+
+window.customElements.define("discover-restaurant-title", RestaurantTitle);
